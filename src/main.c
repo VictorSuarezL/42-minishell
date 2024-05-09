db54e6ad496390c3e386d2df5a9ac0651602383c
VictorSuarezL <vsslledo@gmail.com>
2024-05-09 13:54:37 +0200

    feat: Add ft_strtok_r function to main.c

--- src/main.c
+++ src/main.c
@@ -3,33 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// char	*my_strtok(char *str, const char *delim, char **save_ptr)
-// {
-// 	char	*start;
-// 	char	*end;
-
-// 	if (str != NULL)
-// 		*save_ptr = str;
-// 	else if (*save_ptr == NULL || **save_ptr == '\0')
-// 		return (NULL);
-// 	start = *save_ptr;
-// 	while (*start != '\0' && ft_strchr(delim, *start) != NULL)
-// 		start++;
-// 	if (*start == '\0')
-// 		return (*save_ptr = NULL, NULL);
-// 	end = start;
-// 	while (*end != '\0' && ft_strchr(delim, *end) == NULL)
-// 		end++;
-// 	if (*end != '\0')
-// 	{
-// 		*end = '\0';
-// 		*save_ptr = end + 1;
-// 	}
-// 	else
-// 		*save_ptr = NULL;
-// 	return (start);
-// }
-
 int main()
 {
 	char str[] = "Hola,** mundo   este* * es*un,ejemplo de ft_strtok_r";
