27545b38c28eda10200872dd835e7dd443834d6d
Victor Suarez-Lledo [vsanz-su] <33570867+VictorSuarezL@users.noreply.github.com>
2024-05-31 01:00:29 +0200

    feat: Improve code structure and organization in validator.c

--- includes/minishell.h
+++ includes/minishell.h
@@ -7,17 +7,18 @@
 # include <libft.h>
 
 // NO BORRAR
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <unistd.h>
-# include <readline/history.h>
-# include <readline/readline.h>
 
-int	    ft_strcmp(const char *s1, const char *s2);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*get_delimiter(char **delimiter);
-int	    have_heredoc(char *str, char **delimiter);
+int		have_heredoc(char *str, char **delimiter);
 void	create_file(char *info);
 void	launch_heredoc(char *delimiter);
 char	**ft_split_m(char const *s);
+int		validator(char *str);
 
 #endif
--- src/validator.c
+++ src/validator.c
@@ -1,13 +1,99 @@
 #include <minishell.h>
 
+static int	is_pipe_end(char *str)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(str);
+	if (str[len - 1] == '|')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+static int	quote_counter(char *str)
+{
+	int	i;
+	int	flag;
+	int	flag_a;
+	int	flag_b;
+
+	i = 0;
+	flag = 0;
+	flag_a = 0;
+	flag_b = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' && flag == 0)
+		{
+			flag = 1;
+		}
+		else if (str[i] == '\'' && flag == 1)
+		{
+			flag = 0;
+		}
+		if (str[i] == '\"' && flag == 0)
+		{
+			flag = 1;
+		}
+		else if (str[i] == '\"' && flag == 1)
+		{
+			flag = 0;
+		}
+		i++;
+	}
+	if (flag_a || flag_b)
+	{
+		flag = 1;
+	}
+	return (flag);
+}
+
+static int	is_metacharacter_end(char *str)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(str);
+	if (str[len - 1] == '>' || str[len - 1] == '<')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 
 /**
  * Flag the number of single and double quotes in a given string.
- * 
+ *
  * @param str The string to count quotes in.
  * @return The number of quotes found in the string.
  */
-
-
-
-
+int	validator(char *str)
+{
+	if (quote_counter(str))
+	{
+		return (0);
+	}
+	else if (is_metacharacter_end(str))
+	{
+		return (0);
+	}
+	else if (is_pipe_end(str))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
