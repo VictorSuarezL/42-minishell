ceaf5e6943221a7f7b2d73fbd5dd8927d95e6b93
Victor Sanz Suarez-Lledo <vsanz-su@c3r1s3.42malaga.com>
2024-05-31 16:40:48 +0200

    update

--- src/main.c
+++ src/main.c
@@ -3,12 +3,20 @@
 #include <stdio.h>
 #include <string.h>
 
-
+# define EMPTY 0
+# define CMD 1
+# define ARG 2
+# define TRUNC 3
+# define APPEND 4
+# define INPUT 5
+# define PIPE 6
+# define END 7
 
 int	main(void)
 {
-	t_list	**lst;
-	char	str[100] = "echo -E 'Hola $USER Esto '   es un ejemplo >> miarchivo.txt |";
+	t_list	**tokens;
+	char	str[100] = "echo         -E 'Hola $USER Esto '   es un ejemplo >> miarchivo.txt";
+	// char	str[100] = 'echo "Hola $USER Esto "';
 	char	**args_split;
 	int		i;
 	t_list	*current;
@@ -20,9 +28,9 @@ int	main(void)
 	}
 	
 
-	lst = malloc(sizeof(t_list));
+	tokens = malloc(sizeof(t_list));
 	// char str[100] = "hola \"esto es un ejemplo\" con hola mundo 'como estas' hoy?";
-	printf("ends in pipe? = %d\n", is_pipe_end(str));
+	// printf("ends in pipe? = %d\n", is_pipe_end(str));
 	args_split = ft_split_m(str);
 	i = 0;
 	while (args_split[i] != NULL)
@@ -33,10 +41,10 @@ int	main(void)
 	i = 0;
 	while (args_split[i] != NULL)
 	{
-		ft_lstadd_back(lst, ft_lstnew((void *)args_split[i]));
+		ft_lstadd_back(tokens, ft_lstnew((void *)args_split[i]));
 		i++;
 	}
-	current = *lst;
+	current = *tokens;
 	while (current != NULL)
 	{
 		printf("%s\n", (char *)current->content);
