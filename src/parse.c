19d99dcb8fb47cbe3003d67a7419f252a4c33e25
Victor Sanz Suarez-Lledo <vsanz-su@c3r8s6.42malaga.com>
2024-07-03 16:34:46 +0200

    fixed refactor, first try

--- src/parse.c
+++ src/parse.c
@@ -7,15 +7,12 @@ struct cmd	*nulterminate(struct cmd *cmd)
 	struct redircmd	*rcmd;
 	int				i;
 
-	i = 0;
+	i = -1;
 	if (cmd->type == EXEC)
 	{
 		ecmd = (struct execcmd *)cmd;
-		while (ecmd->argv[i])
-		{
+		while (ecmd->argv[++i])
 			*ecmd->eargv[i] = 0;
-			i++;
-		}
 	}
 	else if (cmd->type == PIPE)
 	{
@@ -34,17 +31,17 @@ struct cmd	*nulterminate(struct cmd *cmd)
 
 struct cmd	*parse_redirs(struct cmd *cmd, char **ps, char *es)
 {
-	int	tok;
+	int		tok;
+	char	*q;
+	char	*eq;
 
-	char *q, *eq;
 	while (peek(ps, es, "<>"))
 	{
 		tok = get_token(ps, es, 0, 0);
 		if (get_token(ps, es, &q, &eq) != 'a')
-			ft_perror("missing file for redirection");
+			ft_perror("Error: missing file for redirection");
 		if (tok == '<')
 		{
-			// cmd = redir_cmd(cmd, q, eq, O_RDONLY, 0);
 			cmd = redir_in_cmd(cmd, q, eq, O_RDONLY);
 		}
 		else if (tok == '>')
@@ -59,30 +56,24 @@ struct cmd	*parse_redirs(struct cmd *cmd, char **ps, char *es)
 	return (cmd);
 }
 
-struct cmd	*parse_exec(char **p_str, char *e_str)
+struct cmd	*parse_exec(char **p_str, char *e_str, int argc)
 {
 	char			*q;
 	char			*end_q;
 	int				tok;
-	int				argc;
 	struct execcmd	*struct_execcmd;
 	struct cmd		*cmd;
 
 	cmd = exec_cmd();
 	struct_execcmd = (struct execcmd *)cmd;
-	argc = 0;
 	cmd = parse_redirs(cmd, p_str, e_str);
 	while (!peek(p_str, e_str, "|"))
 	{
 		tok = get_token(p_str, e_str, &q, &end_q);
 		if (!tok)
-		{
 			break ;
-		}
 		if (tok != 'a')
-		{
 			ft_perror("syntax!\n");
-		}
 		struct_execcmd->argv[argc] = q;
 		struct_execcmd->eargv[argc] = end_q;
 		argc++;
@@ -93,11 +84,39 @@ struct cmd	*parse_exec(char **p_str, char *e_str)
 	return (cmd);
 }
 
+// struct cmd	*parse_exec(char **p_str, char *e_str)
+// {
+// 	char		*q;
+// 	char		*end_q;
+// 	int			tok;
+// 	int			argc;
+// 	struct cmd	*cmd;
+
+// 	cmd = exec_cmd();
+// 	argc = 0;
+// 	cmd = parse_redirs(cmd, p_str, e_str);
+// 	while (!peek(p_str, e_str, "|"))
+// 	{
+// 		tok = get_token(p_str, e_str, &q, &end_q);
+// 		if (!tok)
+// 			break ;
+// 		if (tok != 'a')
+// 			ft_perror("syntax!\n");
+// 		((struct execcmd *)cmd)->argv[argc] = q;
+// 		((struct execcmd *)cmd)->eargv[argc] = end_q;
+// 		argc++;
+// 		cmd = parse_redirs(cmd, p_str, e_str);
+// 	}
+// 	((struct execcmd *)cmd)->argv[argc] = NULL;
+// 	((struct execcmd *)cmd)->eargv[argc] = NULL;
+// 	return (cmd);
+// }
+
 struct cmd	*parse_pipe(char **p_str, char *end_str)
 {
 	struct cmd	*cmd;
 
-	cmd = parse_exec(p_str, end_str);
+	cmd = parse_exec(p_str, end_str, 0);
 	if (peek(p_str, end_str, "|"))
 	{
 		get_token(p_str, end_str, NULL, NULL);
--- src/tokenize.c
+++ src/tokenize.c
@@ -14,156 +14,156 @@
 // 	return (str);
 // }
 
-// char	*skip_token(char *str, char *end)
-// {
-// 	while (str < end && !ft_strchr(" \t\r\n\v", *str) && !ft_strchr("<|>",
-// 			*str))
-// 	{
-// 		str++;
-// 	}
-// 	return (str);
-// }
+char	*skip_token(char *str, char *end)
+{
+	while (str < end && !ft_strchr(" \t\r\n\v", *str) && !ft_strchr("<|>",
+			*str))
+	{
+		str++;
+	}
+	return (str);
+}
 
-// int	handle_redirection(char **aux)
-// {
-// 	int	ret;
+int	handle_redirection(char **aux)
+{
+	int	ret;
 
-// 	ret = **aux;
-// 	(*aux)++;
-// 	if (**aux == '>')
-// 	{
-// 		ret = '+';
-// 		(*aux)++;
-// 	}
-// 	return (ret);
-// }
+	ret = **aux;
+	(*aux)++;
+	if (**aux == '>')
+	{
+		ret = '+';
+		(*aux)++;
+	}
+	return (ret);
+}
 
-// int	handle_quote(char **aux, char *end)
-// {
-// 	char	quote;
+int	handle_quote(char **aux, char *end)
+{
+	char	quote;
 
-// 	quote = **aux;
-// 	(*aux)++;
-// 	// *aux = skip_quote(*aux, end, quote);
-// 	(*aux)++; //TODO TENER EN CUENTA ESTO
-// 	while (*aux < end && **aux != quote)
-// 	{
-// 		(*aux)++;
-// 	}
-// 	if (*aux < end && **aux == quote)
-// 	{
-// 		(*aux)++;
-// 	}
-// 	return ('a');
-// }
+	quote = **aux;
+	(*aux)++;
+	// *aux = skip_quote(*aux, end, quote);
+	(*aux)++; //TODO TENER EN CUENTA ESTO
+	while (*aux < end && **aux != quote)
+	{
+		(*aux)++;
+	}
+	if (*aux < end && **aux == quote)
+	{
+		(*aux)++;
+	}
+	return ('a');
+}
 
-// int	get_special_token(char **aux)
-// {
-// 	int	ret;
+int	get_special_token(char **aux)
+{
+	int	ret;
 
-// 	ret = **aux;
-// 	(*aux)++;
-// 	return (ret);
-// }
+	ret = **aux;
+	(*aux)++;
+	return (ret);
+}
 
-// // Función principal refactorizada
-// int	get_token(char **p_str, char *end_str, char **q, char **end_q)
-// {
-// 	char	*aux;
-// 	int		ret;
-
-// 	ret = 0;
-// 	aux = skip_whitespace(*p_str, end_str);
-// 	if (q)
-// 		*q = aux;
-// 	if (*aux == '\0')
-// 		ret = '\0';
-// 	else if (ft_strchr("|<", *aux))
-// 		ret = get_special_token(&aux);
-// 	else if (*aux == '>')
-// 		ret = handle_redirection(&aux);
-// 	else if (*aux == '"' || *aux == '\'')
-// 		ret = handle_quote(&aux, end_str);
-// 	else
-// 	{
-// 		ret = 'a';
-// 		aux = skip_token(aux, end_str);
-// 	}
-// 	if (end_q)
-// 		*end_q = aux;
-// 	*p_str = skip_whitespace(aux, end_str);
-// 	return (ret);
-// }
-
+// Función principal refactorizada
 int	get_token(char **p_str, char *end_str, char **q, char **end_q)
 {
 	char	*aux;
-	char	quote;
 	int		ret;
 
-	aux = *p_str;
-	while (aux < end_str && ft_strchr(" \t\r\n\v", *aux))
-	{
-		aux++;
-	}
+	ret = 0;
+	aux = skip_whitespace(*p_str, end_str);
 	if (q)
 		*q = aux;
-	ret = *aux;
-	if (!*aux)
-	{
-
-	}
+	if (*aux == '\0')
+		ret = '\0';
 	else if (ft_strchr("|<", *aux))
-	{
-		aux++;
-	}
+		ret = get_special_token(&aux);
 	else if (*aux == '>')
-	{
-		aux++;
-		if (*aux == '>')
-		{
-			ret = '+';
-			aux++;
-		}
-	}
-	else if (*aux == '\'')
-	{
-		int i = 0;
-		aux++;
-		quote = *aux;
-		ret = 'a';
-		aux++;
-		while (aux < end_str && *aux+1 != '\'')
-		{
-			aux++;
-		}
-	}
-	
-	// else if (*aux == '"' || *aux == '\'')
-	// {
-	// 	aux++;
-	// 	quote = *aux;
-	// 	ret = 'a';
-	// 	while (aux < end_str && *aux != quote)
-	// 	{
-	// 		aux++;
-	// 	}
-	// 	if (*aux == quote)
-	// 	{
-	// 		aux++;
-	// 	}
-	// }
+		ret = handle_redirection(&aux);
+	else if (*aux == '"' || *aux == '\'')
+		ret = handle_quote(&aux, end_str);
 	else
 	{
 		ret = 'a';
-		while (aux < end_str && !ft_strchr(" \t\r\n\v", *aux) && !ft_strchr("<|>",
-				*aux))
-			aux++;
+		aux = skip_token(aux, end_str);
 	}
 	if (end_q)
 		*end_q = aux;
-	while (aux < end_str && ft_strchr(" \t\r\n\v", *aux))
-		aux++;
-	*p_str = aux;
+	*p_str = skip_whitespace(aux, end_str);
 	return (ret);
 }
+
+// int	get_token(char **p_str, char *end_str, char **q, char **end_q)
+// {
+// 	char	*aux;
+// 	char	quote;
+// 	int		ret;
+
+// 	aux = *p_str;
+// 	while (aux < end_str && ft_strchr(" \t\r\n\v", *aux))
+// 	{
+// 		aux++;
+// 	}
+// 	if (q)
+// 		*q = aux;
+// 	ret = *aux;
+// 	if (!*aux)
+// 	{
+
+// 	}
+// 	else if (ft_strchr("|<", *aux))
+// 	{
+// 		aux++;
+// 	}
+// 	else if (*aux == '>')
+// 	{
+// 		aux++;
+// 		if (*aux == '>')
+// 		{
+// 			ret = '+';
+// 			aux++;
+// 		}
+// 	}
+// 	else if (*aux == '\'')
+// 	{
+// 		int i = 0;
+// 		aux++;
+// 		quote = *aux;
+// 		ret = 'a';
+// 		aux++;
+// 		while (aux < end_str && *aux+1 != '\'')
+// 		{
+// 			aux++;
+// 		}
+// 	}
+	
+// 	// else if (*aux == '"' || *aux == '\'')
+// 	// {
+// 	// 	aux++;
+// 	// 	quote = *aux;
+// 	// 	ret = 'a';
+// 	// 	while (aux < end_str && *aux != quote)
+// 	// 	{
+// 	// 		aux++;
+// 	// 	}
+// 	// 	if (*aux == quote)
+// 	// 	{
+// 	// 		aux++;
+// 	// 	}
+// 	// }
+// 	else
+// 	{
+// 		ret = 'a';
+// 		while (aux < end_str && !ft_strchr(" \t\r\n\v", *aux) && !ft_strchr("<|>",
+// 				*aux))
+// 			aux++;
+// 	}
+// 	if (end_q)
+// 		*end_q = aux;
+// 	while (aux < end_str && ft_strchr(" \t\r\n\v", *aux))
+// 		aux++;
+// 	*p_str = aux;
+// 	return (ret);
+// }
