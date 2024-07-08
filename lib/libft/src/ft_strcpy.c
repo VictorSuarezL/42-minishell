a4a35e30fcf48022642710f89bfab0c233b4345b
Francisco Fernandez Leon <francfer@c3r8s4.42malaga.com>
2024-07-08 17:17:42 +0200

    libft normi

--- lib/libft/src/ft_strcpy.c
+++ lib/libft/src/ft_strcpy.c
@@ -1,15 +1,16 @@
 #include "libft.h"
 
-char *ft_strcpy(char *dest, const char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
-    char *orig_dest;
+	char	*orig_dest;
 
-    orig_dest = dest;
-    while (*src) {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return (orig_dest);
+	orig_dest = dest;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (orig_dest);
 }
--- lib/libft/src/ft_strstr.c
+++ lib/libft/src/ft_strstr.c
@@ -1,24 +1,24 @@
 #include "libft.h"
 
-char *ft_strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-    const char *h;
-    const char *n;
+	const char	*h;
+	const char	*n;
 
-    if (!*needle)
-        return ((char *)haystack);
-    while (*haystack)
-    {
-        h = haystack;
-        n = needle;
-        while (*h && *n && *h == *n)
-        {
-            h++;
-            n++;
-        }
-        if (!*n)
-            return ((char *)haystack);
-        haystack++;
-    }
-    return (NULL);
+	if (!*needle)
+		return ((char *)haystack);
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+		while (*h && *n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (!*n)
+			return ((char *)haystack);
+		haystack++;
+	}
+	return (NULL);
 }
