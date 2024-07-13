3fcdbc27c191a1ba271033aada9a60eaf654ddce
Fran <user@users-MacBook-Pro.local>
2024-07-13 10:35:05 +0200

    exit_builtin non numeric working

--- src/exit_builtin.c
+++ src/exit_builtin.c
@@ -1,26 +1,33 @@
 #include <minishell.h>
 
-void	exit_stat(char *buf, int *exit_code, char ***copy_en, char ***copy_exp)
-{
-	int	should_exit;
-
-	should_exit = 0;
-	if (ft_strncmp(buf, "exit", 4) == 0 && (buf[4] == ' ' || buf[4] == '\0'))
-	{
-		*exit_code = exit_builtin(buf, &should_exit);
-		if (should_exit)
-		{
-			final_clean(*copy_en, *copy_exp);
-			exit(*exit_code);
-		}
-	}
-}
-
 void	init_args(char *buf, char **arg1, char **arg2, char **command)
 {
 	*command = ft_strtok(buf, " ");
 	*arg1 = ft_strtok(NULL, " ");
 	*arg2 = ft_strtok(NULL, " ");
+}
+
+int	is_digit_str(char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+int handle_exit_no_args(int *should_exit)
+{
+    ft_printf("exit\n");
+    *should_exit = 1;
+    return 0;
+}
+int handle_exit_with_invalid_arg(char *arg1)
+{
+    ft_printf("exit\n");
+    ft_printf("exit: %s: numeric argument required\n", arg1);
+    return 2;
 }
 
 int	exit_builtin(char *input, int *should_exit)
@@ -35,18 +42,18 @@ int	exit_builtin(char *input, int *should_exit)
 	init_args(buf, &arg1, &arg2, &command);
 	*should_exit = 0;
 	if (arg1 == NULL)
-	{
-		ft_printf("exit\n");
-		*should_exit = 1;
-		return (0);
-	}
+		return (handle_exit_no_args(should_exit));
 	else if (arg2 == NULL)
 	{
+		if (!is_digit_str(arg1))
+			return (handle_exit_with_invalid_arg(arg1));
 		ft_printf("exit\n");
 		*should_exit = 1;
 		return (ft_atoi(arg1));
 	}
 	else
+	{
 		return (1);
+	}
 	return (1);
 }
